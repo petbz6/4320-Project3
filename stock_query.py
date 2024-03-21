b31a059bd6b4392c508606fe09c500925d83bc61
petbz6 <92735934+petbz6@users.noreply.github.com>
2024-03-21 16:08:51 -0500

    Modified step 1 to return data

--- stock_query.py
+++ stock_query.py
@@ -2,18 +2,19 @@ import requests
 
 # API KEY: C70QWUENAT21S0SB
 
-while True:
-    stock_symbol_input = input("Enter the stock symbol you are looking for: ").upper()
+def get_stock_data():
+    while True:
+        stock_symbol_input = input("Enter the stock symbol you are looking for: ").upper()
 
-    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={stock_symbol_input}&interval=5min&apikey=C70QWUENAT21S0SB"
-    
-    r = requests.get(url)
-    if r.status_code == 200:
-        data = r.json()
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={stock_symbol_input}&interval=5min&apikey=C70QWUENAT21S0SB'
+        r = requests.get(url)
         
-        if "Error Message" in data:
-            print("Invalid stock symbol. Please enter a valid stock symbol.")
-        else:
-            print(data)
-            break
-    
+       
+        if r.status_code == 200:
+            data = r.json()
+            if "Error Message" in data:
+                print("Invalid stock symbol. Please enter a valid stock symbol.")
+            else:
+                return data
+
+stock_data = get_stock_data()
